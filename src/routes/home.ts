e11e4e7ae0bac9a09eca93852539b7015c7015db
OwaisSiddiqui <GitHubGIt18888*>
2021-02-27 01:09:05 -0500

    Change from res.send to res.json

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -4,18 +4,14 @@ const router: express.Router = express.Router()
 
 const isUserLoggedIn = (req: any, res: any, next: any) => {
     if (req.session.user) {
-        res.send("Welcome " + req.session.user + ".")
+        res.json({"message": "Welcome " + req.session.user + "."})
     } else {
         next()
     }
 }
 
 router.get('/', isUserLoggedIn, (req, res) => {
-    res.send("Welcome to Twitter! To signup, please go to the /signup route. If you already have an account, please go to the /login route.")
-})
-
-router.get('/', (req, res) => {
-
+    res.json({"message": "Welcome to Twitter! To signup, please go to the /signup route. If you already have an account, please go to the /login route."})
 })
 
 export default router
