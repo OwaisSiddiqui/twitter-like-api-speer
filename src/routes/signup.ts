c8063409f92c59a10ea4e22c939af3dbcd6e335c
OwaisSiddiqui <GitHubGIt18888*>
2021-02-27 01:09:33 -0500

    Change from res.send to res.json

--- src/routes/signup.ts
+++ src/routes/signup.ts
@@ -4,12 +4,12 @@ import signupUser from '../util/signupUser'
 
 const router: express.Router = express.Router()
 
-const verifyBody = (body: any) => {
-    return typeof body.username == "string" &&  typeof body.password == "string" && body.username && body.password
+const verifyBody = (body: any) => {    
+    return "username" in body && "password" in body && Object.keys(body).length == 2 && typeof body.username == "string" &&  typeof body.password == "string" && body.username && body.password
 }
 
 router.get('/', (req, res) => {
-    res.send("Send a POST request to this route with a username and password to signup.")
+    res.json({"message": "Send a POST request to this route with a username and password to signup."})
 })
 
 router.post('/', async (req: any, res: any) => {
@@ -26,19 +26,19 @@ router.post('/', async (req: any, res: any) => {
                     res.redirect('/')
                 } else {
                     res.status(500)
-                    res.send("Internal server error. Please contact server administrators to resolve the issue.")
+                    res.json({"message": "Internal server error. Please contact server administrators to resolve the issue."})
                 }
             } else {
                 res.status(409)
-                res.send("Username is already taken. Please choose another username and try again.")
+                res.json({"message": "Username is already taken. Please choose another username and try again."})
             }
         } else {
             res.status(422)
-            res.send("Either missing username, password parameters or parameters are not of type string.")
+            res.json({"message": "Either missing username, password parameters or parameters are not of type string."})
         }
     } else {
         res.status(400)
-        res.send("Cannot signup while having a logged in session. Please logout first.")
+        res.json({"message": "Cannot signup while having a logged in session. Please logout first."})
     }
 })
 
