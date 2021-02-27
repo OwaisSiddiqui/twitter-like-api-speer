1941dd237ccf8c3f833b7a875783a3d5139b6c1a
OwaisSiddiqui <GitHubGIt18888*>
2021-02-26 22:22:47 -0500

    Update to add home and login route

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,44 @@
 require('dotenv').config()
 import express from 'express'
-
-import connectToDatabase from './mongodb'
+import session from 'express-session'
+import redis from 'redis'
+import connectToDatabase from './util/mongodb'
 import signupRoute from './routes/signup'
+import loginRoute from './routes/login'
+import homeRoute from './routes/home'
 
 const app: express.Express = express()
 const port: number = parseInt(process.env.PORT ? process.env.PORT : "3000")
+const host: string = process.env.HOST ? process.env.HOST : "localhost"
+const redisClient = redis.createClient();
+const redisPort: number = parseInt(process.env.REDIS_PORT ? process.env.REDIS_PORT : "6379")
+const sessionStore = require('connect-redis')(session);
+const redisTtl: number = 86400
 
+redisClient.on('connect', () => {
+    console.log("Redis is connected.")
+})
+
+redisClient.on('ready', () => {
+    console.log("Redis is ready.")
+})
+
+redisClient.on('error', (error: Error) => {
+    console.log('Redis error: ', error);
+});
+
+app.use(session({
+    secret: '8Jy#Y#PGDK#?xnPr',
+    resave: false,
+    saveUninitialized: false,
+    name: 'sid',
+    cookie: {
+        maxAge: 1 * 60 * 1000,
+        sameSite: true,
+        secure: false 
+    },
+    store: new sessionStore({ host: host, port: redisPort, client: redisClient, ttl: redisTtl })
+}))
 app.use(express.json())
 app.use((error: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
     if (error instanceof SyntaxError) {
@@ -14,14 +46,12 @@ app.use((error: Error, req: express.Request, res: express.Response, next: expres
         res.send("Invalid JSON body.")
     }
 });
+app.use('/', homeRoute)
 app.use('/signup', signupRoute)
+app.use('/login', loginRoute)
 
 connectToDatabase()
 
-app.get("/", (req, res) => {
-    res.send("Welcome to Twitter! To signup, please go to the /signup route. If you already have an account, please go to the /login route.")
-})
-
-app.listen(port, "localhost", () => {
+app.listen(port, host, () => {
     console.log("Connected to server.")
 })
